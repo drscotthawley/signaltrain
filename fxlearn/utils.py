bffad6f874ea61cb54dd54440bc434ef100a6faa
Scott Hawley <scott.hawley@belmont.edu>
2018-04-18 02:11:48 -0500

    added creation of variety of input signals & effects

--- fxlearn/utils.py
+++ fxlearn/utils.py
@@ -12,7 +12,10 @@ import librosa
 import os
 from multiprocessing.pool import ThreadPool as Pool
 from functools import partial
+import scipy.signal as signal
 
+
+#----- General program utilities
 
 # my cheap way of showing loss 'graphically':  "*" farther right means larger loss (tilt your head to the right)
 def print_loss(loss_val, log=True):
@@ -26,47 +29,6 @@ def print_loss(loss_val, log=True):
     return
 
 
-
-def gen_pitch_shifted_pair(input_sigs, target_sigs, fs, amp_fac, freq_fac, num_waves, batch_index):
-    sig_length = input_sigs.shape[1]
-    for n in range(num_waves):
-        # randomize the signal
-        amp = 0.2*np.random.rand()    # stay bounded well below 1.0
-        freq = 2 * np.pi * ( 400 + 400*np.random.rand() )
-
-        # learn the adaptive filter for the following input -> target pair: different amp, freq & phase
-        input_sigs[batch_index]  +=           amp * torch.cos(           freq * torch.arange(sig_length) / fs)
-        target_sigs[batch_index] += amp_fac * amp * torch.sin(freq_fac * freq * torch.arange(sig_length) / fs)
-    return
-
-
-# currently creates pitch-shifted collections of sine waves
-def make_signals(sig_length, fs=44100., amp_fac=0.43, freq_fac=0.35, num_waves=20, batch_size=20, parallel=True):
-
-    input_sigs = torch.zeros((batch_size,sig_length))
-    target_sigs = torch.zeros((batch_size,sig_length))
-
-    # generate them in parallel threads that all share the input_sigs and target_sigs arrays
-    batch_indices = tuple( range(batch_size) )
-    if (parallel):
-        pool = Pool()
-        pool.map( partial(gen_pitch_shifted_pair, input_sigs, target_sigs, fs, amp_fac, freq_fac, num_waves), batch_indices)
-        pool.close()
-        pool.join()
-    else:
-        for batch_index in batch_indices:
-            gen_pitch_shifted_pair(input_sigs, target_sigs, fs, amp_fac, freq_fac, num_waves, batch_index)
-
-    input_var = Variable(input_sigs)
-    target_var = Variable(target_sigs, requires_grad=False)
-    if torch.has_cudnn:
-        input_var = input_var.cuda()
-        target_var = target_var.cuda()
-
-    return input_var, target_var
-
-
-
 def save_checkpoint(state, is_best, filename='checkpoint.pth.tar'):
     torch.save(state, filename)
     if is_best:
@@ -74,7 +36,7 @@ def save_checkpoint(state, is_best, filename='checkpoint.pth.tar'):
 
 
 
-def make_report(input_var, target_var, wave_form, outfile=None, epoch=None, show_input=False, device=''):
+def make_report(input_var, target_var, wave_form, outfile=None, epoch=None, show_input=False, device='', diff=True):
     wave_form = wave_form.squeeze(1).data.cpu().numpy()[0, :]
     input_sig = input_var.squeeze(1).data.cpu().numpy()[0, :]
     target_sig = target_var.squeeze(1).data.cpu().numpy()[0, :]
@@ -82,12 +44,17 @@ def make_report(input_var, target_var, wave_form, outfile=None, epoch=None, show
     print('MSE = ', mse)      # Print mean squared error
 
     fig = plt.figure(figsize=(11,8.5),dpi=120)
-    if (show_input):
-        plt.plot(input_sig, 'b-', label='Input')
-    plt.plot(target_sig, 'r-', label='Target')
-    plt.plot(wave_form, 'g-', label='Output')
+    ampmax = 0.5
+    if (diff):
+        diffval = wave_form-target_sig
+        plt.plot(diffval, 'b-', label='Output-Target')
+        ampmax = 1.1*np.max(diffval)
+    else:
+        if (show_input):
+            plt.plot(input_sig, 'b-', label='Input')
+        plt.plot(target_sig, 'r-', label='Target')
+        plt.plot(wave_form, 'g-', label='Output')
 
-    ampmax = 1.0
     plt.ylim((-ampmax,ampmax))   # zoom in
     plt.legend()
 
@@ -106,5 +73,222 @@ def make_report(input_var, target_var, wave_form, outfile=None, epoch=None, show
     librosa.output.write_wav('progress'+device+'_input.wav', input_sig, sr)
     librosa.output.write_wav('progress'+device+'_output.wav', wave_form, sr)
     librosa.output.write_wav('progress'+device+'_target.wav', target_sig, sr)
+
+#----------- End general program utilities
+
+
+
+#------------------  Audio Utilities
+def read_audio_file(filename):
+    sig, fs = librosa.load(filename)
+    if (len(sig.shape) > 1):   # convert stereo to mono by throwing out right channel
+        return sig[0], fs
+    else:
+        return sig, fs
+
+# generate pitch-shifted pair, on one processor
+def psp_oneproc(input_sigs, target_sigs, fs, amp_fac, freq_fac, num_waves, chunk_index):
+    sig_length = input_sigs.shape[1]
+    for n in range(num_waves):
+        # randomize the signal
+        amp = 0.2*np.random.rand()    # stay bounded well below 1.0
+        freq = 2 * np.pi * ( 400 + 400*np.random.rand() )
+
+        # learn the adaptive filter for the following input -> target pair: different amp, freq & phase
+        input_sigs[chunk_index]  +=           amp * torch.cos(           freq * torch.arange(sig_length) / fs)
+        target_sigs[chunk_index] += amp_fac * amp * torch.sin(freq_fac * freq * torch.arange(sig_length) / fs)
+    return
+
+def gen_pitch_shifted_pairs(chunk_size, fs, amp_fac, freq_fac, num_waves, num_chunks):
+        input_sigs = torch.zeros((batch_size, chunk_size))
+        target_sigs = torch.zeros((batch_size, chunk_size))
+        # generate them in parallel threads that all share the input_sigs and target_sigs arrays
+        chunk_indices = tuple( range(num_chunks) )
+        if (parallel):
+            pool = Pool()
+            pool.map( partial(psp_oneproc, input_sigs, target_sigs, fs, amp_fac, freq_fac, num_waves), chunk_indices)
+            pool.close()
+            pool.join()
+        else:
+            for chunk_index in chunk_indices:
+                psp_oneproc(input_sigs, target_sigs, fs, amp_fac, freq_fac, num_waves, chunk_index)
+        return input_sigs, target_sigs
+
+
+def gen_input_sample(t, chooser=None):
+    x = np.copy(t)*0.0
+    if (chooser is None):
+        chooser = np.random.randint(0,7)
+    #chooser = 6
+    #print("make_input_signal: chooser = ",chooser)
+    if (0 == chooser):                 # "bunch of spikes"
+        n_spikes = 100
+        for i in range(n_spikes):   # arbitrarily make a 'spike' somewhere, surrounded by silence
+          loc = int(np.random.random()*len(t)-2)+1
+          height = np.random.random()-0.5    # -0.5...0.5
+          x[loc] = height
+          x[loc+1] = height/2  # widen the spike a bit
+          x[loc-1] = height/2
+        x = x + 0.1*np.random.normal(0.0,scale=0.1,size=x.size)    # throw in noise
+        return x
+    elif (1 == chooser):                  # "pluck"
+        amp0 = (0.6*np.random.random()+0.3)*np.random.choice([-1,1])
+        t0 = np.random.random()*0.3
+        decay = 8*np.random.random()
+        freq = 300*np.random.random()
+        x = amp0*np.exp(-decay * (t-t0) ) * np.sin(freq* (t-t0))
+        x[np.where(t < t0)] = 0
+        return x
+    elif (2 == chooser):                # 'box'
+        height = (0.3*np.random.random()+0.2)*np.random.choice([-1,1])
+        x = height*np.ones(t.shape[0])
+        t1 = np.random.random()/2
+        t2 = t1 + np.random.random()/2
+        x[np.where(t<t1)] = 0.0
+        x[np.where(t>t2)] = 0.0
+        #x += 0.01
+        return x
+    elif (3 == chooser):                # ramp up then down
+        height = (0.4*np.random.random()+0.2)*np.random.choice([-1,1])
+        width = 0.3*np.random.random()/4   # half-width actually
+        t0 = 2*width + 0.4*np.random.random() # make sure it fits
+        x = height* ( 1 - np.abs(t-t0)/width )
+        x[np.where(t < (t0-width))] = 0
+        x[np.where(t > (t0+width))] = 0
+        #x += 0.01
+        return x
+    elif (4 == chooser):
+        amp = 0.4+0.4*np.random.random()
+        freq = 30*np.random.random()    # sin, with random start & freq
+        t0 = np.random.random()
+        x = amp*np.sin(freq*(t-t0))
+        x[np.where(t < t0)] = 0.0
+        return x
+    elif (5 == chooser):                # fixed sine wave
+        freq = 5+150*np.random.random()
+        amp = 0.4+0.4*np.random.random()
+        global global_freq
+        global_freq = freq
+        return amp*np.sin(freq*t)
+    elif (6 == chooser):                # white noise
+        amp = 0.2+0.2*np.random.random()
+        #amp = 2.0*np.random.random()
+        x = amp*(2*np.random.random(t.shape[0])-1)
+        return x
+    else:
+        x= 0.5*(make_input_signal(t)+make_input_signal(t)) # superposition of previous
+        return x
+    return np.copy(t)   # failsafe return just in case of typo above
+
+
+def lowpass(x, fc_fac=1.0): # low pass filter
+	fc = fc_fac / len(x)
+	b, a = signal.butter(1, fc, analog=False)
+	zi = signal.lfilter_zi(b, a)
+	z, _ = signal.lfilter(b, a, x, zi=zi*x[0])
+	return z
+
+def echo(x, delay_samples=1487, echoes=2, ratio=0.6, dtype=np.float):
+    # ratio = redution ratio
+    y = np.copy(x).astype(dtype)
+    for i in range(echoes):
+	    ip1 = i+1
+	    delay_length = ip1 * delay_samples
+	    x_delayed = np.roll(x, delay_length)
+	    x_delayed[0:delay_length] = 0
+	    y += pow(ratio, ip1) * x_delayed
+    return y
+
+# simple compressor, thanks to Eric Tarr
+def compressor(x, thresh=-20, ratio=3, fc_fac=5.0, dtype=np.float):
+	fc = fc_fac / len(x)                         # this is like 1/attack time
+	b, a = signal.butter(1, fc, analog=False)
+	zi = signal.lfilter_zi(b, a)
+	dB = 10*np.log10(np.abs(x) + 1e-8)
+	in_env, _ = signal.lfilter(b, a, dB, zi=zi*dB[0])  # input envelope calculation
+	out_env = np.copy(in_env).astype(dtype)               # output envelope
+	i = np.where(in_env >  thresh)          # compress where input env exceeds thresh
+	out_env[i] = thresh + (in_env[i]-thresh)/ratio
+	gain = np.power(10.0,(out_env-in_env)/10)
+	y = np.copy(x) * gain
+	return y
+
+
+def functions(x, f='id'):  # function to be learned
+	if ('id' == f):
+		return x 						# identity function
+	elif ('x^2'==f):
+		return x**2   					# given an x on [0,1], this should work
+	elif ('clip' == f):
+		return np.clip(x,-0.3,0.3)  	# hard limiter, clips signal
+	elif ('sin' == f):
+		return np.sin(4*x)              # just made this up
+	elif ('dec_cos' == f):
+		y = np.exp(-2 * x ) * np.cos(40* x) # decaying cosine
+		return y
+	elif ('wiggle' == f):
+		y  = np.exp(-1*(x+0.7))*np.cos(20*x)			# decaying cos  wave
+		y = y + np.exp(-40*(x-0.5)**2)					# plus  gaussian
+		return y
+	# low pass filter
+	elif ('lpf' ==f):
+		return lowpass(f)
+	elif ('delay' == f) or ('echo' == f):
+		return echo(x)
+	elif ('comp' == f):
+		return compressor(x)
+	else:
+		print("functions: error invalid type")
+
+
+
+# Note the chopnstack pair uses numpy instead of pytorch.  you can convert later
+def chopnstack(sig, size=8192, dtype=np.float):
+    # Chop'n'Stack!  Cuts up a signal into chunks and stacks them vertically
+    sig_len = len(sig)
+    n = int(np.ceil(sig_len*1.0/size))             # number of chunks, round up
+    buffer = np.zeros(n * size).astype(dtype)   # for zero-pad, embed in larger array
+    buffer[0:sig_len] = sig
+    return np.vstack(np.split(buffer, n))
+
+def inv_chopnstack(stack, orig_len=None):
+    # Inverse of Chop'n'Stack: takes vertical stack and produces 1-D signal
+    if (None == orig_len):
+        return stack.reshape(-1)
+    else:
+        return stack.reshape(-1)[0:orig_len]
+
+
+def gen_data(sig_length, chunk_size=8192):
+    my_dtype = np.float
+
+    # Generate input signal
+    print("\nGenerating input data...")
+    input_sig = np.random.rand(sig_length).astype(my_dtype)-0.5   # start with random noise
+    t = np.linspace(0,1,num=chunk_size).astype(my_dtype)
+    num_sample_clips = int(sig_length / chunk_size * 0.8)  # just overwrite some random amount
+    for i in range(num_sample_clips):
+        start_ind = int( np.random.rand()*(sig_length-chunk_size) )
+        input_sig[start_ind:start_ind + chunk_size] = gen_input_sample(t)
+
+    input_sig *= 0.5    # just make it smaller to avoid any clipping that may occur
+
+    # Apply the effect, whatever it is
+    effect = 'echo'
+    print("Applying '",effect,"' effect...",sep="")
+    target_sig = functions(input_sig, f=effect)
+
+    # chop up the input & target signal
+    input_stack = torch.from_numpy( chopnstack(input_sig, size=chunk_size) ).float()
+    target_stack = torch.from_numpy( chopnstack(target_sig, size=chunk_size) ).float()
+
+    input_var = Variable(input_stack)
+    target_var = Variable(target_stack, requires_grad=False)
+    if torch.has_cudnn:
+        input_var = input_var.cuda()
+        target_var = target_var.cuda()
+
+    print("Leaving with input_stack.size() = ",input_stack.size())
+    return input_var, target_var
 
 # EOF
--- train_fxlearn.py
+++ train_fxlearn.py
@@ -11,49 +11,54 @@ import os
 import fxlearn as fxl    # This is where I'm putting things...
 
 def train_model(model, optimizer, criterion, start_epoch = 0, max_epochs=100,
-    batch_size=100, sig_length=8192, fs=44100.,
-    tol=1e-14, change_every=10, save_every=100, plot_every=200):
+    batch_size=100, sig_length=8192*20000, fs=44100.,
+    tol=1e-14, change_every=10000, save_every=10, plot_every=10):
+
+    input_var, target_var = fxl.utils.gen_data(sig_length)
 
     for epoch_iter in range(max_epochs - start_epoch):
         epoch = start_epoch + epoch_iter
         print('Epoch ', epoch,'/', max_epochs, sep="", end=":")
 
         # change the input dataset to encourage generality
-        if (0 == epoch_iter % change_every):
-            input_var, target_var = fxl.utils.make_signals(sig_length, fs, batch_size=batch_size)   # generate numpy arrays
+        if (epoch > 0) and (0 == epoch_iter % change_every):
+            input_var, target_var = fxl.utils.gen_data(sig_length)
 
-        optimizer.zero_grad()
-        wave_form = model(input_var)   # run the neural network forward
-        loss = criterion(wave_form, target_var)
+        n_batches = 1#input_var.size()[0]
+        for batch in range(n_batches):
+            batch_index = batch * batch_size
 
-        loss_val = loss.data.cpu().numpy()[0]
-        fxl.utils.print_loss(loss_val)
+            optimizer.zero_grad()
+            wave_form = model(input_var)   # run the neural network forward
+            loss = criterion(wave_form, target_var)
 
-        if (epoch > start_epoch):
-            device = str(torch.cuda.current_device())
-            if (0 == epoch % save_every) or (epoch >= max_epochs-1):
-                checkpoint_name = 'checkpoint'+device+'.pth.tar'
-                print("Saving checkpoint in ",checkpoint_name)
-                fxl.utils.save_checkpoint({'epoch': epoch + 1, 'state_dict': model.state_dict(),
-                    'optimizer' : optimizer.state_dict(),}, False, filename=checkpoint_name)
-            if (0 == epoch % plot_every):
-                outfile = 'progress'+device+'.pdf'
-                print("Saving progress report to ",outfile,": ",end="")
-                fxl.utils.make_report(input_var, target_var, wave_form, outfile=outfile, epoch=epoch, device=device)
+            loss_val = loss.data.cpu().numpy()[0]
+            fxl.utils.print_loss(loss_val)
 
-        if loss.data.cpu().numpy() < tol:
-            break
+            if (epoch > start_epoch):
+                device = str(torch.cuda.current_device())
+                if (0 == epoch % save_every) or (epoch >= max_epochs-1):
+                    checkpoint_name = 'checkpoint'+device+'.pth.tar'
+                    print("Saving checkpoint in ",checkpoint_name)
+                    fxl.utils.save_checkpoint({'epoch': epoch + 1, 'state_dict': model.state_dict(),
+                        'optimizer' : optimizer.state_dict(),}, False, filename=checkpoint_name)
+                if (0 == epoch % plot_every):
+                    outfile = 'progress'+device+'.pdf'
+                    print("Saving progress report to ",outfile,": ",end="")
+                    fxl.utils.make_report(input_var, target_var, wave_form, outfile=outfile, epoch=epoch, device=device)
 
-        loss.backward(retain_graph=True)      # retain_graph=True needed for RNNs, it seems
-        optimizer.step()
+            if loss.data.cpu().numpy() < tol:
+                break
+
+            loss.backward(retain_graph=True)      # retain_graph=True needed for RNNs, it seems
+            optimizer.step()
 
     return model
 
 
-
 # One additional model evaluation on new 'test; data
-def eval_model(model, sig_length, fs):
-    input_var, target_var = fxl.utils.make_signals(sig_length, fs, num_waves=4)
+def eval_model(model, sig_length):
+    input_var, target_var = fxl.utils.gen_data(sig_length)
     wave_form = model(input_var)    # run network forward
     outfile = 'final.pdf'
     print("Saving final Test evaluation report to",outfile,": ",end="")
@@ -61,7 +66,9 @@ def eval_model(model, sig_length, fs):
     return
 
 
-
+#-------------------------------------------------------------------------------
+# MAIN BLOCK
+#-------------------------------------------------------------------------------
 def main():
     # useful for ensuring similar initializations when testing different models
     torch.manual_seed(1)
@@ -71,10 +78,10 @@ def main():
     # Parse command line arguments
     parser = argparse.ArgumentParser(description='fxlearn')
     parser.add_argument('--epochs', default=10000, type=int, help="Number of iterations to train for")
-    parser.add_argument('--length', default=8192, type=int, help="Length of audio signals")
+    parser.add_argument('--length', default=8192*2000, type=int, help="Length of audio signals")
     parser.add_argument('--fs', default=44100, type=int, help="Sample rate in Hertz")
     parser.add_argument('--device', default=0, type=int, help="CUDA device to use (e.g. 0 or 1)")
-    parser.add_argument('--change', default=10, type=int, help="Changed data every this many epochs")
+    parser.add_argument('--change', default=10000, type=int, help="Changed data every this many epochs")
 
     parser.add_argument('--model', default='spectral', type=str,
                     help="Model type: 'spectral' (default) or 'seq2seq'")
@@ -85,6 +92,9 @@ def main():
     sig_length = args.length
     fs = args.fs
 
+    #--------------------
+    # Check CUDA status
+    #--------------------
     if torch.has_cudnn:
         torch.cuda.set_device(args.device)
         name = torch.cuda.get_device_name(args.device)
@@ -92,17 +102,21 @@ def main():
     else:
         print('No CUDA')
 
-
+    #------------------------------------
     # Set up model and training criteria
+    #------------------------------------
     if ('seq2seq' == args.model):
         model = fxl.models.Seq2Seq()
     else:
         model = fxl.models.SpecEncDec()
     criterion = torch.nn.MSELoss()
-    optimizer = torch.optim.Adam([ {'params': model.parameters()}], lr = 0.0003,  amsgrad=True)#, weight_decay=0.01)
+    # on next line, setting eps as per pytorch Issue #1767
+    optimizer = torch.optim.Adam([ {'params': model.parameters()}], lr = 0.0005, eps=1e-7)#,  amsgrad=True)#, weight_decay=0.01)
 
 
-
+    #---------------------
+    # Checkpoint recovery
+    #---------------------
     if os.path.isfile(args.resume):     # checkpoint recovery
         print("=> loading checkpoint '{}'".format(args.resume))
         checkpoint = torch.load(args.resume)
@@ -116,15 +130,22 @@ def main():
         print("=> no checkpoint found at '{}'".format(args.resume))
         start_epoch = 0
 
-
+    #------------------------------------------------------------------
+    # Once checkpoints are loaded (or not), CUDA-ify model if possible
+    #------------------------------------------------------------------
     if torch.has_cudnn:
         model.cuda()
+    model = torch.nn.DataParallel(model)
 
+    #---------------
     # Training Loop
+    #---------------
     model = train_model(model, optimizer, criterion, start_epoch=start_epoch, max_epochs=args.epochs,
         fs=fs, sig_length=sig_length, change_every=args.change)
 
-    # Show performance on test data:
+    #--------------------------------
+    # Evaluate model on Test dataset
+    #--------------------------------
     eval_model(model, sig_length, fs)
     return
 
