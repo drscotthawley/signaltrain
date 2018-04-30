685e30ca37bf2d53370bae585e62a5cbaed22f00
Scott Hawley <scott.hawley@belmont.edu>
2018-04-30 00:43:05 -0500

    added dataset and dataloader but still not working

--- signaltrain/audio.py
+++ signaltrain/audio.py
@@ -13,7 +13,7 @@ from torch.utils.data.dataset import Dataset
 import pysox
 #import matplotlib.pyplot as plt   # just for debugging
 
-# Note: torchaudio is also a thing! requires sox. See http://pytorch.org/audio/,  https://github.com/pytorch/audio
+# Note: torchaudio is also a thing! requires sox. See http://pytorch.org/audio/, https://github.com/pytorch/audio
 
 
 
@@ -271,25 +271,28 @@ class Limiter:
 
 
 
-# This writes to a file, processes it via sox, and reads back the output files
-# requires that sox and pysox are installed
-#  effect params is a string, such as
-#        effectparams=[ ("vol", [ b'18dB' ]), ]
-# See the sox docs for more info: http://sox.sourceforge.net/sox.html#EFFECTS
-def apply_sox_effect(signal, sr, fxstr):
+def apply_sox_effect(signal, fxstr, sr=44100):
+    # This writes signal to a .wav file, processes it sox to another file, loads that and returns it.
+    #
+    # signal:  a numpy list of numbers; the audio signal
+    # sr:      the sample rate in Hz, must be an integer
+    # fxstr:   a semicolon-separated string starting with the effect name followed by parameter values in order
+    #          e.g., "lowpass;500" or "vol;18dB" or 'compand;0.3,0.8;-50', or if you're feeling ambitious,..
+    #                fxstr='ladspa;/usr/lib/ladspa/mbeq_1197.so;mbeq;-2;-3;-3;-6;-9;-9;-10;-8;-6;-5;-4;-3;-1;0;0;0'
+    #          See the sox docs for more on effects: http://sox.sourceforge.net/sox.html#EFFECTS
+    #          (Why semicolon-separated?  Because sox looks for both commas and colons!)
     inpath, outpath = 'in.wav', 'out.wav'
-    librosa.output.write_wav(inpath, signal, sr)         # write the input audio to a file
-
-    tmp = fxstr.split(',')
+    librosa.output.write_wav(inpath, signal, sr)                     # write the input audio to a file
+    tmp = fxstr.split(';')
     fxname = tmp[0]
     fxvals = [str.encode(x) for x in tmp[1:]]
     effectparams = [(fxname, fxvals),]
+    print("effectparams=",effectparams)
     app = pysox.CSoxApp(inpath, outpath, effectparams=effectparams)   # apply the sox effect & get new file
     app.flow()
 
-    out_signal, sr = librosa.load(outpath, sr)
-    return out_signal
-
+    out_signal, sr =  librosa.load(outpath, sr=sr)
+    return out_signal[0:len(signal)]
 
 
 '''-----------------------------------------------------------------------------
@@ -324,35 +327,56 @@ def functions(x, f='id'):                # function to be learned
         y = limiter.limit(x, 0.5)
         limiter = None                           # forced garbage collection.
         return y
-    elif ('sox,' in f):                           # it's a sox effect, parse a comma-separated string
-        fxstr = f.replace('sox,' , '')            # e.g. f = "sox,lowpass,500"; ignore the "sox," part
-        print(" Calling apply_sox_effect with effectparams =",effectparams,', len(x) =',len(x))
+    elif ('od' == f):   # really dumb/simple overdrive: a tanh!
+        a = 3.0
+        y = np.tanh(a*x)
+        return y
+    elif ('sox;' in f):                           # it's a sox effect, parse a semicolon-separated string
+        fxstr = f.replace('sox;' , '')            # e.g. f = "sox,lowpass,500"; ignore the "sox;" part
+        print(" Calling apply_sox_effect with fxstr =",fxstr,', len(x) =',len(x))
         y = apply_sox_effect(x, fxstr=fxstr)
         print("....and we're back, len(y) = ",len(y))
         return y
     else:
-        print("functions: error invalid type")
-        assert(False)  # probably more graceful ways to exit, but I want to know immediately
+        raise ValueError("functions: error invalid type: "+f)
 
 
 
 
-# Chop'n'Stack!  Cuts up a signal into chunks and stacks them vertically
-def chopnstack(sig, size=8192, dtype=np.float32):
-    # Note the chopnstack pair uses numpy instead of pytorch.  you can convert later
+# Chop'n'Stack!  Cuts up a signal into chunks and stacks them vertically. Pad with zeros
+def chopnstack(sig, chunk_size=8192):
+    # Note the chopnstack pair uses numpy instead of pytorch.  you can convert later with _from_numpy()
     sig_len = len(sig)
-    n = int(np.ceil(sig_len*1.0/size))             # number of chunks, round up
-    buffer = np.zeros(n * size).astype(dtype)   # for zero-pad, embed in larger array
-    buffer[0:sig_len] = sig
-    return np.vstack(np.split(buffer, n))
+    rows = int(np.ceil(sig_len*1.0/chunk_size))          # number of chunks, round up
+    num_to_pad = rows*chunk_size - sig_len
+    sig = np.pad(sig,(0,num_to_pad),'constant')
+    return np.reshape(sig, (-1, chunk_size))
+    #buffer = np.zeros(n * size).astype(dtype)   # for zero-pad, embed in larger array
+    #buffer[0:sig_len] = sig
+    #return np.vstack(np.split(buffer, n))
 
 # Inverse of Chop'n'Stack: takes vertical stack and produces 1-D signal
+#  fastest method is ravel  https://ipython-books.github.io/45-understanding-the-internals-of-numpy-to-avoid-unnecessary-array-copying/
+#      ravel is faster than flatten because flatten sometimes copies and ravel never copies
 def inv_chopnstack(stack, orig_len=None):
-    if (None == orig_len):
-        return stack.reshape(-1)
-    else:
-        return stack.reshape(-1)[0:orig_len]
+    return stack.ravel()
+    #if (None == orig_len):
+    #    return stack.reshape(-1)
+    #else:
+    #    return stack.reshape(-1)[0:orig_len]
 
+
+# mu laws from https://gist.github.com/lirnli/4282fcdfb383bb160cacf41d8c783c70
+def encode_mu_law(x, mu=256):
+    mu = mu-1
+    fx = np.sign(x)*np.log(1+mu*np.abs(x))/np.log(1+mu)
+    return np.floor((fx+1)/2*mu+0.5).astype(np.long)
+
+def decode_mu_law(y, mu=256):
+    mu = mu-1
+    fx = (y-0.5)/mu*2-1
+    x = np.sign(fx)/mu*((1+mu)**np.abs(fx)-1)
+    return x
 
 
 '''---------------------------------------------------------------------------------------
@@ -369,9 +393,10 @@ def inv_chopnstack(stack, orig_len=None):
                                 before generating new data. (It'd be nice to be able to
                                 do everything 'in place', but w/ PyTorch vs. numpy, and
                                 GPU vs CPU, this can get complicated.)
+        mu_law:      Set to true to apply mu-law encoding to input & target audio
 ---------------------------------------------------------------------------------------'''
-def gen_audio(sig_length, chunk_size=8192, effect='ta', input_var=None, target_var=None):
-    my_dtype = np.float32
+def gen_audio(sig_length, chunk_size=8192, effect='ta', input_var=None, target_var=None, mu_law=False):
+    dtype = np.float32
 
     # Free up pytorch tensor memory usage before generating new data
     if (input_var is not None) and (target_var is not None):
@@ -391,17 +416,22 @@ def gen_audio(sig_length, chunk_size=8192, effect='ta', input_var=None, target_v
         # Generate input signal
         clips_per_chunk = 4
         clip_size = int( chunk_size / clips_per_chunk)
-        input_sig = np.zeros(sig_length).astype(my_dtype)
+        input_sig = np.zeros(sig_length).astype(dtype)       # allocate storage
         #input_sig +=  (2*np.random.rand(sig_length)-1)*1e-6  # just a little noise to help it not be zero
-        t = np.linspace(0,1,num=clip_size).astype(my_dtype)
-        num_sample_clips = int(sig_length / clip_size)  # just overwrite some random amount
-        for i in range(num_sample_clips):
+        t = np.linspace(0,1,num=clip_size).astype(dtype)
+        num_sample_clips = int(sig_length / clip_size)
+        for i in range(num_sample_clips):               #  go through each section of input and assign a waveform
             start_ind = i * clip_size
             sample_type = 2  # 'pluck'
             input_sig[start_ind:start_ind + clip_size] = gen_input_sample(t,chooser=sample_type)
 
+        # normalize the input audio to have max amplitude of 0.5; large values are problematic for the input
+        input_sig = 0.5 * input_sig / np.max(input_sig)
+
+
         if ('delay' == effect):
-            input_sig *= 0.5    # for delay, just make it smaller to avoid any clipping that may occur
+            input_sig *= 0.5    # for delay, just make it even smaller to avoid any clipping that may occur
+
 
         #print("Plotting input_sig:")
         #fig = plt.figure()
@@ -411,9 +441,16 @@ def gen_audio(sig_length, chunk_size=8192, effect='ta', input_var=None, target_v
         # Apply the effect, whatever it is
         target_sig = functions(input_sig, f=effect)
 
+        if mu_law:
+            input_sig = encode_mu_law(input_sig)
+            target_sig = encode_mu_law(target_sig)
+            dtype = np.long
+
         # chop up the input & target signal
-        input_stack = torch.from_numpy( chopnstack(input_sig, size=chunk_size) )
-        target_stack = torch.from_numpy( chopnstack(target_sig, size=chunk_size) )
+        input_stack =  chopnstack(input_sig, chunk_size=chunk_size)
+        target_stack = chopnstack(target_sig, chunk_size=chunk_size)
+        input_stack = torch.from_numpy( input_stack )
+        target_stack = torch.from_numpy( target_stack )
 
     input_var = Variable(input_stack)
     target_var = Variable(target_stack, requires_grad=False)
@@ -425,27 +462,95 @@ def gen_audio(sig_length, chunk_size=8192, effect='ta', input_var=None, target_v
     return input_var, target_var
 
 
+# from the PyTorch Data Loading & Processing tutorial
+#  http://pytorch.org/tutorials/beginner/data_loading_tutorial.html
+class ToTensor(object):
+    """Convert ndarrays in sample to Tensors."""
+    def __call__(self, sample):
+        input, target = sample['input'], sample['target']
+        return {'input': torch.from_numpy(input),
+                'target': torch.from_numpy(target)}
+
+# like chop'n'stack, above but for PyTorch Data
+class Chunkify(object):
+    """Breaks signals up into chunks"""
+    def __init__(self, chunk_size, dtype=np.float32):
+        self.chunk_size = chunk_size
+        self.dtype = dtype
+        self.num_chunks = int(sig_length / chunk_size)
+
+    def __call__(self, sample):
+        input, target = sample['input'], sample['target']
+        return {'input': input.view(self.num_chunks, self.chunk_size),
+                'target': input.view(self.num_chunks, self.chunk_size) }
+
 
 
 # TODO: skeleton for future Dataset class for Pytorch Dataloaders.  Not finished.
-#    Reading from http://pytorch.org/audio/datasets.html
+#    Reading from http://pytorch.org/tutorials/beginner/data_loading_tutorial.html
+#  (torchaudio doesn't install or I'd use that  http://pytorch.org/audio/datasets.html
+# ...this will replace gen_audio, above
 class AudioDataset(Dataset):
-    def __init__(self, sig_length, chunk_size=8192, effect='ta', input_var=None, target_var=None):
+    def __init__(self, sig_length, chunk_size=8192, effect='ta', input_var=None, target_var=None, dtype=np.float32):
         # function is where the initial logic happens like reading a csv, assigning transforms etc.
         # and allocating memory
+        self.chunk_size = chunk_size
+        self.effect = effect
         self.num_chunks = int(sig_length / chunk_size)
+        self.sig_length =  chunk_size * self.num_chunks  # this will pad with zeros to make sure everything fits
 
+        self.effect = effect
+
+        # allocate storage for input signal and output signal
+        #  this will get chopped up into chunks via a reshape
+        self.input_sig = np.zeros(self.sig_length).astype(dtype)
+        self.target_sig = self.input_sig.copy()
+
+        # set up some variables for generating randome signals
+        self.clips_per_chunk = 4
+        self.clip_size = int( self.chunk_size / self.clips_per_chunk)
+        self.t = np.linspace(0,1,num= self.clip_size)   # time values used by the generator equations
+
+        self.input_var = None
+        self.target_var = None
+
+        #self.input_stack = torch.zeros((num_chunks, chunk_size))
+        self.gen_new()  # provide some data for starters
 
     def __getitem__(self, index):
         # function returns the data and labels. This function is called from dataloader like this:
         #      img, label = MyCustomDataset.__getitem__(99)  # For 99th item
         # this is where torchvision transforms are often used
-        return (img, label)
+        sample = {'input': self.input_var, 'target': self.target_var}
+        return sample
 
     def __len__(self):
-        return self.num_chunks # of how many examples(images?) you have
+        return self.input_var.size()[0] # how many examples you have
 
+    def gen_new(self):   # this generates new data and places the results in the vars used by getitem
+        num_sample_clips = int(self.sig_length / self.clip_size)
+        for i in range(num_sample_clips):               #  go through each section of input and assign a waveform
+            start_ind = i * self.clip_size
+            sample_type = 2  # 'pluck'
+            self.input_sig[start_ind:start_ind + self.clip_size] = gen_input_sample(self.t, chooser=sample_type)
 
+        # normalize the input audio to have max amplitude of 0.5; large values are problematic for the input
+        self.input_sig = 0.5 * self.input_sig / np.max(self.input_sig)
 
+        if ('delay' == self.effect):
+            self.input_sig *= 0.5    # for delay, just make it even smaller to avoid any clipping that may occur
+
+        # Apply the effect, whatever it is
+        self.target_sig = functions(self.input_sig, f=self.effect)
+
+        self.input_sig = chopnstack( self.input_sig, chunk_size=self.chunk_size)
+        self.target_sig = chopnstack( self.target_sig, chunk_size=self.chunk_size)
+
+        self.input_var = torch.from_numpy(self.input_sig)
+        self.target_var = torch.from_numpy(self.target_sig) #, requires_grad=False)
+        #if torch.has_cudnn:
+        #    self.input_var = self.input_var.cuda()
+        #    self.target_var = self.target_var.cuda()
+        return
 
 # EOF
