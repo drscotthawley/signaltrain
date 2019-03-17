94b7e66e0afb140498b2a0763990e18e28062570
Scott Hawley <scott.hawley@belmont.edu>
2019-03-17 16:36:39 -0500

    renaming data to datasets

--- signaltrain/train.py
+++ signaltrain/train.py
@@ -9,8 +9,18 @@ import torch.nn as nn
 from torch.utils.data import DataLoader
 import os, sys
 import time
-from signaltrain import audio, io_methods, learningrate, data, loss_functions, misc
+from signaltrain import audio, io_methods, learningrate, datasets, loss_functions, misc
 from signaltrain.nn_modules import nn_proc
+
+
+try:   # NVIDIA Apex for mixed-precision training
+    from apex.parallel import DistributedDataParallel as DDP
+    from apex.fp16_utils import *
+    from apex import amp, optimizers
+    from apex.multi_tensor_apply import multi_tensor_applier
+except ImportError:
+    raise ImportError("Please install apex from https://www.github.com/nvidia/apex to run this code.")
+
 
 
 def train(effect=audio.Compressor_4c(), epochs=100, n_data_points=200000, batch_size=20,
@@ -46,26 +56,30 @@ def train(effect=audio.Compressor_4c(), epochs=100, n_data_points=200000, batch_
     chunk_size, out_chunk_size = model.in_chunk_size, model.out_chunk_size
     y_size = out_chunk_size
     print("Model defined.  Number of trainable parameters:",sum(p.numel() for p in model.parameters() if p.requires_grad))
+    model.to(device)
 
     # Specify learning rate schedule...although we don't bother stepping the momentum
-    lr_max = 8.0e-4                         # obtained after running helpers.learningrate.lr_finder.py
-    lrs = learningrate.get_1cycle_schedule(lr_max=lr_max, n_data_points=n_data_points, epochs=epochs, batch_size=batch_size)
+    lr_max = 1.0e-4                         # obtained after running helpers.learningrate.lr_finder.py
+    lr_sched = learningrate.get_1cycle_schedule(lr_max=lr_max, n_data_points=n_data_points, epochs=epochs, batch_size=batch_size)
+    maxiter = len(lr_sched)
+    eps_sched = 1e-8 + 0.5-0.5*np.cos(np.linspace(0, np.pi, maxiter))
 
     # Initialize optimizer. given our "random" training data, weight decay seem to doesn't help but rather slows training way down
-    optimizer = torch.optim.Adam(list(model.parameters()), lr=lrs[0], weight_decay=0)
+    optimizer = torch.optim.Adam(list(model.parameters()), lr=lr_sched[0], weight_decay=0)
+
 
     # Are we synthesizing data or do we expect it to come from files
     synth_data = datapath is None
 
     # Setup/load data
     if synth_data:  # synthesize data  # TODO: warning: broke this a while ago in favor of using files.
-        dataset = data.SynthAudioDataSet(chunk_size, effect, sr=sr, datapoints=n_data_points)
-        dataset_val = data.SynthAudioDataSet(chunk_size, effect, sr=sr, datapoints=n_data_points//4, recycle=True)
+        dataset = datasets.SynthAudioDataSet(chunk_size, effect, sr=sr, datapoints=n_data_points, y_size=out_chunk_size)
+        dataset_val = datasets.SynthAudioDataSet(chunk_size, effect, sr=sr, datapoints=n_data_points//4, recycle=True, y_size=out_chunk_size)
     else: # use files
-        dataset = data.AudioFileDataSet(chunk_size, effect, sr=sr,  datapoints=n_data_points, path=datapath+"/Train/",  y_size=out_chunk_size, rerun=False)
-        dataset_val = data.AudioFileDataSet(chunk_size, effect, sr=sr, datapoints=n_data_points//4, path=datapath+"/Val/", y_size=out_chunk_size, rerun=False)
+        dataset = datasets.AudioFileDataSet(chunk_size, effect, sr=sr,  datapoints=n_data_points, path=datapath+"/Train/",  y_size=out_chunk_size, rerun=False)
+        dataset_val = datasets.AudioFileDataSet(chunk_size, effect, sr=sr, datapoints=n_data_points//4, path=datapath+"/Val/", y_size=out_chunk_size, rerun=False)
 
-    dataloader = DataLoader(dataset, batch_size=batch_size, num_workers=10, shuffle=True, worker_init_fn=data.worker_init) # need worker_init for more variance
+    dataloader = DataLoader(dataset, batch_size=batch_size, num_workers=10, shuffle=True, worker_init_fn=datasets.worker_init) # need worker_init for more variance
     dataloader_val = DataLoader(dataset_val, batch_size=batch_size, num_workers=10, shuffle=False)
 
 
@@ -77,12 +91,20 @@ def train(effect=audio.Compressor_4c(), epochs=100, n_data_points=200000, batch_
         # TODO: set optimizer state from checkpoint
 
 
+    # mixed precision
+    # Initialize Amp.  Amp accepts either values or strings for the optional override arguments,
+    # for convenient interoperation with argparse.
+    model, optimizer = amp.initialize(model, optimizer, opt_level="O2")
+
+
     # Copy model to (other) GPU if possbible
-    parallel = torch.cuda.device_count() > 1
+    parallel = False # torch.cuda.device_count() > 1
     if parallel:       # For Hawley's 2 Titan X GPUs this typically cuts execution time down by ~30% (not 50%)
         print("Replicating NN model for data-parallel execution across", torch.cuda.device_count(), "GPUs")
         model = nn.DataParallel(model)
-    model.to(device)             # put the model on the GPU if it wasn't already
+
+    #model = model.half()
+    #model.to(device)             # put the model on the GPU if it wasn't already
 
     # Set up for using additional L1 regularization - scale by frequency bin
     scale_by_freq = None
@@ -104,21 +126,24 @@ def train(effect=audio.Compressor_4c(), epochs=100, n_data_points=200000, batch_
         # Training phase
         model.train()
         for x, y, knobs in dataloader:
-            x_cuda, y_cuda, knobs_cuda = x.to(device), y.to(device), knobs.to(device) # TODO: does DataLoader already do this?
+            # move to GPU TODO: does DataLoader already do this?
+            x_cuda, y_cuda, knobs_cuda = x.to(device), y.to(device), knobs.to(device)
 
-            lr = lrs[min(iter_count, len(lrs)-1)]  # get value for learning rate
+            lr = lr_sched[min(iter_count, len(lr_sched)-1)]  # get value for learning rate
+            eps = eps_sched[min(iter_count, len(lr_sched)-1)]
             data_point += batch_size
 
             # feed-forward synthesis
             y_hat, mag, mag_hat = model.forward(x_cuda, knobs_cuda)
 
+            #print("y_hat, mag, mag_hat = ",y_hat, mag, mag_hat)
             # set up loss weighting
             if scale_by_freq is None:
                 expfac = 7./mag_hat.size()[-1]   # exponential L1 loss scaling by ~1000 times (30dB) over freq range
                 scale_by_freq = torch.exp(expfac* torch.arange(0., mag_hat.size()[-1])).expand_as(mag_hat)
 
             # loss evaluation
-            loss = loss_functions.calc_loss(y_hat, y_cuda, mag_hat, scale_by_freq=scale_by_freq)
+            loss = loss_functions.calc_loss(y_hat.float(), y_cuda.float(), mag_hat.float(), scale_by_freq=scale_by_freq.float())
 
             # Status message
             batch_num += 1
@@ -130,17 +155,28 @@ def train(effect=audio.Compressor_4c(), epochs=100, n_data_points=200000, batch_
 
             # Optimization
             optimizer.zero_grad()
-            loss.backward()
-            # Clip norm
+
+            #loss.backward()   # replace this with amp call
+            with amp.scale_loss(loss, optimizer) as scaled_loss:
+                scaled_loss.backward()
+            torch.nn.utils.clip_grad_norm_(amp.master_params(optimizer), max_norm=1., norm_type=1)
+
+            # Clip model norm
+            '''
             if parallel:
                 for child in model.children():  # for DataParallel
-                    child.clip_grad_norm_()
+                    if hasattr(child, 'clip_grad_norm_'):
+                        child.clip_grad_norm_()
             else:
-                model.clip_grad_norm_()   # for non-DataParallel
+                if hasattr(model, 'clip_grad_norm_'):
+                    model.clip_grad_norm_(optimizer)   # for non-DataParallel
+            '''
+
             optimizer.step()
 
             # Apply Learning rate scheduling
             optimizer.param_groups[0]['lr'] = lr     # adjust according to schedule
+            #optimizer.param_groups[0]['eps'] = eps
             iter_count += 1
         # end of loop over training batches. Training phase finished for this epoch
 
@@ -153,7 +189,7 @@ def train(effect=audio.Compressor_4c(), epochs=100, n_data_points=200000, batch_
             x_val_cuda, y_val_cuda, knobs_val_cuda = x_val.to(device), y_val.to(device), knobs_val.to(device)
 
             y_val_hat, mag_val, mag_val_hat = model.forward(x_val_cuda, knobs_val_cuda)
-            loss_val = loss_functions.calc_loss(y_val_hat, y_val_cuda, mag_val_hat)
+            loss_val = loss_functions.calc_loss(y_val_hat.float(), y_val_cuda, mag_val_hat.float())
             vl_avg = beta*vl_avg + (1-beta)*loss_val.item()    # (running) average val loss
             if 0 == val_batch_num % status_every:
                 timediff = time.time() - first_time
@@ -173,6 +209,13 @@ def train(effect=audio.Compressor_4c(), epochs=100, n_data_points=200000, batch_
         # save checkpoint of model to file, which can be loaded later
         if ((epoch+1) % cp_every == 0):
             misc.save_checkpoint(checkpointname, model, epoch, parallel, optimizer, effect, sr)
+
+        # time estimate (got tired of calcuting this by hand every time!)
+        if ((epoch+1)==1):
+            secs_left = (time.time() - first_time) * (epochs-1)
+            future = time.time() + secs_left
+            hours =  secs_left / 3600.0
+            print(f"\nExpect run to finish in roughly {hours:.1f} hours, on {time.ctime(future)}")
 
     print("\nTotal elapsed time =",time.time() - first_time)
     return None
