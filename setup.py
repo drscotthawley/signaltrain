100272bfed9b26845f6dbced5c7f09823cedab0c
Scott Hawley <scott.hawley@belmont.edu>
2019-07-24 01:06:51 +0100

    added setup.py to enable remote installation

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+import setuptools
+from distutils.core import setup
+
+setup(name='signaltrain',
+      version='1.0',
+      description='learning nonlinear time-dependent audio effects',
+      author='Scott Hawley',
+      author_email='scott.hawley@belmont.edu',
+      url='https://www.signaltrain.ml',
+      packages=['signaltrain'],
+     )
